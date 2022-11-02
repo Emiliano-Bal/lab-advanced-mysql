7b96538d7fe8e52b65b65ff30a340e1aa9fa427b
Emiliano-Bal <emilianobalcazard@gmail.com>
2022-11-02 14:07:03 -0600

    Problem 1

---
+++ your-code/solution.sql
@@ -0,0 +1,18 @@
+SHOW DATABASES;
+USE publications;
+SHOW TABLES;
+
+SELECT * FROM roysched;
+
+select royalty_calc.au_id, round(sum(royalty_calc.sales_royalty) + royalty_calc.advance, 1) AS PROFIT
+from (
+select  ta.au_id, ta.title_id, t.advance, t.price * s.qty * (t.royalty/100) * (ta.royaltyper/100) sales_royalty
+from titleauthor ta
+inner join titles t on ta.title_id = t.title_id
+inner join sales s on ta.title_id = s.title_id) royalty_calc
+group by royalty_calc.title_id, royalty_calc.au_id
+order by PROFIT desc
+limit 3;
+
+
+
