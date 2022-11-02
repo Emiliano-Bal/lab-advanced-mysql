60ef11f296dafb86c50933e9a259c5975729558f
Emiliano-Bal <emilianobalcazard@gmail.com>
2022-11-02 14:10:22 -0600

    Finished

--- your-code/solution.sql
+++ your-code/solution.sql
@@ -15,4 +15,29 @@ order by PROFIT desc
 limit 3;
 
 
+-- Challenge 2 - Alternative Solution
+create temporary table publications.author_profit
+select ta.au_id, ta.title_id, s.qty, t.price, t.advance, t.royalty, ta.royaltyper, t.price * s.qty * (t.royalty/100) * (ta.royaltyper/100) sales_royalty
+from titleauthor ta
+inner join titles t on ta.title_id = t.title_id
+inner join sales s on ta.title_id = s.title_id;
+
+select ap.au_id, ap.title_id, round(sum(ap.sales_royalty), 2) + ap.advance profit
+from author_profit ap
+group by ap.au_id, ap.title_id
+order by profit desc
+limit 3;
+
+-- Challenge 3
+
+create table publications.most_profiting_authors
+select ap.au_id, sum(ap.sales_royalty) + ap.advance profit
+from author_profit ap
+group by ap.au_id, ap.title_id
+order by profit desc;
+ 
+SELECT * FROM publications.most_profiting_authors;
+
+
+
 
